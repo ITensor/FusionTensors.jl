b35757bd2b14f20ef80fdb8bec279e48f752381d
Olivier Gauthé <ogauthe@flatironinstitute.org>
2024-12-13 12:22:09 +0100

    cache unitary coeff

--- src/permutedims/unitaries.jl
+++ src/permutedims/unitaries.jl
@@ -5,7 +5,11 @@ using LRUCache: LRU
 
 using SymmetrySectors: quantum_dimension
 
-const unitary_cache = LRU{Any,AbstractMatrix}(; maxsize=10000)
+const unitary_cache = LRU{
+  Tuple{FusionTree,FusionTree,FusionTree,FusionTree,Tuple{Vararg{Int}}},Float64
+}(;
+  maxsize=10000,  # TBD size
+)
 
 # ======================================  Interface  =======================================
 function compute_unitary(
@@ -14,23 +18,26 @@ function compute_unitary(
   return compute_unitary_clebsch_gordan(new_ft, old_ft, flatperm)
 end
 
-function unitary_key(codomain_legs, domain_legs, old_outer_block, biperm)
-  legs = (codomain_legs..., domain_legs...)
-  old_arrows = isdual.(legs)
-  old_sectors = ntuple(i -> blocklabels(legs[i])[old_outer_block[i]], length(legs))
-  return (old_arrows, old_sectors, length(codomain_legs), biperm)
-end
-
 # ===========================  Constructor from Clebsch-Gordan  ============================
 function overlap_fusion_trees(
-  old_trees::Tuple{FusionTree,FusionTree},
-  new_trees::Tuple{FusionTree,FusionTree},
+  old_trees::Tuple{FusionTree{S},FusionTree{S}},
+  new_trees::Tuple{FusionTree{S},FusionTree{S}},
   flatperm::Tuple{Vararg{Integer}},
-)
+) where {S}
   old_proj = contract_singlet_projector(old_trees...)
   new_proj = contract_singlet_projector(new_trees...)
   a = contract((), new_proj, flatperm, old_proj, ntuple(identity, ndims(new_proj)))
   return a[] / quantum_dimension(root_sector(first(new_trees)))
+end
+
+function cached_unitary_coeff(
+  old_trees::Tuple{FusionTree{S},FusionTree{S}},
+  new_trees::Tuple{FusionTree{S},FusionTree{S}},
+  flatperm::Tuple{Vararg{Integer}},
+) where {S}
+  get!(unitary_cache, (old_trees..., new_trees..., flatperm)) do
+    overlap_fusion_trees(old_trees, new_trees, flatperm)
+  end
 end
 
 function compute_unitary_clebsch_gordan(
@@ -43,10 +50,8 @@ function compute_unitary_clebsch_gordan(
     old_outer = Tuple(findblock(old_ft, old_trees...))
     swapped_old_block = Block(getindex.(Ref(Tuple(old_outer)), flatperm))
     for new_trees in keys(trees_block_mapping(new_ft))
-      new_outer = findblock(new_ft, new_trees...)
-      if swapped_old_block == new_outer
-        unitary[old_trees, new_trees] = overlap_fusion_trees(old_trees, new_trees, flatperm)
-      end
+      swapped_old_block != findblock(new_ft, new_trees...) && continue
+      unitary[old_trees, new_trees] = cached_unitary_coeff(old_trees, new_trees, flatperm)
     end
   end
   return unitary
